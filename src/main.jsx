3a7caa37ec75a69a590525b59c7c6d7c71fd8f8f
Yijin Ro <yri.carmine@gmail.com>
2025-10-03 17:00:48 -0400

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
    <StrictMode>
-    <BrowserRouter basename="/gameInvite">
+    <HashRouter basename="/gameInvite">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
